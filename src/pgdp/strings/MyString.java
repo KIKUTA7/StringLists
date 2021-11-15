a18f3fefaf5f7bb0263c909691f8a2f34ea325d4
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-15 13:29:42 +0400

    dfb

--- src/pgdp/strings/MyString.java
+++ src/pgdp/strings/MyString.java
@@ -1,1 +1,20 @@
 package pgdp.strings;
+
+public class MyString {
+    private  char [] data;
+    private  MyString next = null;
+    public MyString (char [] data)
+    {
+        if(data!=null)
+        this.data = data;
+        next = this;
+    }
+    public int length ()
+    {
+        int l = 0;
+        for (MyString t =this;t!=null;t=t.next)
+            l+=this.data.length;
+        return l;
+    }
+
+}
