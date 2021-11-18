2426c3dc5d684e4a14c2415a58df0edfcde836e4
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-18 23:23:15 +0400

    khssdbcjsd

--- src/pgdp/strings/Main.java
+++ src/pgdp/strings/Main.java
@@ -4,10 +4,9 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        MyString str = new MyString(new char[]{'d','b','c','d'});
-        str.concat(new char[]{'e','f'});
-        MyString str1 = new MyString(new char[]{'d','b'});
-        str1.concat(new char[]{'c','d','e','f'});
-        System.out.println(str.equals(str1));
+        MyString str1 = new MyString(new char[]{'a','b','c','d'});
+        str1.concat(new char[]{'e','f'});
+        MyString str2 = new MyString(new char[]{'a','b','c','d','e','f'});
+        System.out.println(str1.equals(str2));
     }
 }
